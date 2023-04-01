e4d69ee302c2c3fcc69839941bde8c5ce3c807e3
Ahmed Nabil <94ahmed.nabil@gmail.com>
2023-04-01 21:38:19 +0200

    upper buttons functionality

--- app.js
+++ app.js
@@ -5,3 +5,43 @@ window.onload = setTimeout(() => {
   preloader.style.display = "none";
   container.style.display = "block";
 }, 1000);
+
+let addCloseButton = (parentElement)=>{
+  let closeButton = document.createElement("span");
+  closeButton.className = "close-button";
+  let closeButtonText = document.createTextNode("X");
+  closeButton.appendChild(closeButtonText);
+  parentElement.appendChild(closeButton);
+}
+// Create help popup
+document.querySelector(".image-2").addEventListener("click", () => {
+  container.style.opacity = "0.3";
+  let popupHelp = document.createElement("div");
+  popupHelp.className = "popup-help";
+  let p = document.createElement("p");
+  let pText = document.createTextNode("Help content goes here.");
+  p.appendChild(pText);
+  popupHelp.appendChild(p);
+  addCloseButton(popupHelp)
+  document.body.appendChild(popupHelp);
+});
+
+// Create dummy image popup
+document.querySelector(".image-1").addEventListener("click", () => {
+  container.style.opacity = "0.3";
+  let popupImage = document.createElement("div");
+  popupImage.className = "popup-image";
+  let image = document.createElement("img");
+  image.src = "./assets/dummy.jpg";
+  popupImage.appendChild(image);
+  addCloseButton(popupImage)
+  document.body.appendChild(popupImage);
+});
+
+// Close the popups
+document.addEventListener("click", (e) => {
+  if (e.target.className == "close-button") {
+    e.target.parentNode.remove();
+    container.style.opacity = "1";
+  }
+});
